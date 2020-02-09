c18b8773cb4762625bc0676501954bb8bac2721e
Drew Switzer <switzer.36@gmail.com>
2020-02-09 11:42:54 -0500

    Solve Array Manipulation challenge

---
+++ arrays/array-manipulation/array-manipulation.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the arrayManipulation function below.
+function arrayManipulation(n, queries) {
+    let slopeArr = new Array(n);
+    slopeArr.fill(0);
+    let max = null;
+
+    queries.forEach(arr => {
+        slopeArr[arr[0] - 1] += arr[2];
+        slopeArr[arr[1]] += -arr[2];
+    });
+
+    let newTotal = 0;
+    slopeArr.forEach(slope => {
+        newTotal += slope;
+        if(max == null || newTotal > max)
+            max = newTotal;
+    });
+
+    return max;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nm = readLine().split(' ');
+
+    const n = parseInt(nm[0], 10);
+
+    const m = parseInt(nm[1], 10);
+
+    let queries = Array(m);
+
+    for (let i = 0; i < m; i++) {
+        queries[i] = readLine().split(' ').map(queriesTemp => parseInt(queriesTemp, 10));
+    }
+
+    let result = arrayManipulation(n, queries);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
